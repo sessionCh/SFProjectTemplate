ee05985b776a12d0257071b01bddc5c9cf895812
sessionCh <641331737@qq.com>
2016-04-18 11:08:33 +0800

    Initial Commit

---
+++ SFProjectTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SFProjectTemplate
+//
+//  Created by sessionCh on 16/4/18.
+//  Copyright © 2016年 www.sunfobank.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SFProjectTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SFProjectTemplate
+//
+//  Created by sessionCh on 16/4/18.
+//  Copyright © 2016年 www.sunfobank.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
